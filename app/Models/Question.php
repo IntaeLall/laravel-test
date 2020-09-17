f0422f16c39489d5aa206eacef89f3358302e1ee
Brownbass <islall3@googlemail.com>
2020-09-17 17:51:29 +0100

    Add relationship to Question and Questionire models

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Questionnaire;
 
 class Question extends Model
 {
-    //
+    /**
+     * @return BelongsTo
+     */
+    public function questionnaire(): BelongsTo
+    {
+        return $this->has(Questionnaire::class);
+    }
 }
--- app/Models/Questionnaire.php
+++ app/Models/Questionnaire.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Questionnaire extends Model
 {
     protected $fillable = ['name'];
+
+    /**
+     * @return HasMany
+     */
+    public function questionnaire(): HasMany
+    {
+        return $this->has(Questionnaire::class);
+    }
 }
