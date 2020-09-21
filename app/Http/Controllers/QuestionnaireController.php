56e76ff8e5ed38429c7d000a070d8a267d316c91
Brownbass <islall3@googlemail.com>
2020-09-21 03:43:50 +0100

    Correct types

--- app/Http/Controllers/QuestionnaireController.php
+++ app/Http/Controllers/QuestionnaireController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Questionnaire;
 use App\Http\Resources\QuestionnaireResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class QuestionnaireController extends Controller
 {
@@ -11,14 +12,20 @@ class QuestionnaireController extends Controller
      * Display the specified resource.
      *
      * @param Questionnaire $$questionnaire
-     * @return ApiResource
+     * @return ResourceCollection
      */
-    public function index()
+    public function index(): ResourceCollection
     {
         $questionnaires = Questionnaire::paginate(10);
         return QuestionnaireResource::collection($questionnaires);
     }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param Questionnaire $$questionnaire
+     * @return QuestionnaireResource
+     */
     public function show(Questionnaire $questionnaire): QuestionnaireResource
     {
         return new QuestionnaireResource($questionnaire->loadMissing('questions'));
